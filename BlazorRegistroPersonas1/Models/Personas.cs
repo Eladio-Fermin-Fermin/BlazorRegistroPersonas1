cbd015fe77c528a787ff7b7230d46b4c40c12051
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-01-23 09:52:22 -0400

    Se creo la Clase personas.

---
+++ BlazorRegistroPersonas1/Models/Personas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorRegistroPersonas1.Models
+{
+    public class Personas
+    {
+        [Key]
+        public int PersonaId { get; set; }
+
+        public string Nombres { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Cedula { get; set; }
+
+        public string Direccion { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+    }
+}
