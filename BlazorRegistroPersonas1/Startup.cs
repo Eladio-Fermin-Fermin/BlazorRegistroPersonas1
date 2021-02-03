ebce565e1639bd67119c754157e2d62856cd0787
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-02-03 14:16:52 -0400

    .

--- BlazorRegistroPersonas1/Startup.cs
+++ BlazorRegistroPersonas1/Startup.cs
@@ -36,7 +36,7 @@ namespace BlazorRegistroPersonas1
             services.AddSingleton<WeatherForecastService>();
             services.AddDbContext<Contexto>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))); services.AddBlazoredToast();
             //Se inyecto la BLL.
-            services.AddTransient<PersonasBLL>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
